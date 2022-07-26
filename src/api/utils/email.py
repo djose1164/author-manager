0dc1b4cf7972934d486689d26c5141a4752fd17d
Daniel Victoriano <djose1164@gmail.com>
2022-07-26 19:19:04 -0400

    Create email module

---
+++ src/api/utils/email.py
@@ -0,0 +1,17 @@
+from flask_mail import Mail, Message
+from flask import current_app
+
+mail = Mail()
+
+
+def send_email(to, subject, template):
+    mail.send(
+        Message(
+            subject,
+            recipients=[
+                to,
+            ],
+            html=template,
+            sender=current_app.config["MAIL_DEFAULT_SENDER"],
+        )
+    )
